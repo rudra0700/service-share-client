05b5a698a9a1a8bbd24807cb8e44fcfd9ce484f3
rudra0700 <baruarudra7@gmail.com>
2024-12-26 20:58:23 +0600

    footer added

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import Home from '../pages/Home';
 import Navbar from '../components/Navbar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer';
+
 
 const MainLayout = () => {
     return (
         <div className="container mx-auto">
             <Navbar></Navbar>
             <Outlet></Outlet>
+             <div className='mt-24'>
+
+            <Footer></Footer>
+             </div>
         </div>
     );
 };
